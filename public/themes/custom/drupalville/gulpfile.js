1ba30ff9117e574641d6307b112054e77abb3176
Jin Trang <hu.trang@gmail.com>
2016-06-29 14:48:28 -0400

    added gulpfile with various tasks
    - minify js
    - sass globbing
    - converting sass to css
    - watch
    - browser sync
    - default

---
+++ public/themes/custom/drupalville/gulpfile.js
@@ -0,0 +1,73 @@
+'use strict';
+
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var notify = require('gulp-notify');
+var jshint = require('gulp-jshint');
+var sass = require('gulp-sass');
+var globbing = require('gulp-sass-globbing');
+var browserSync = require('browser-sync').create();
+
+// Minify JS
+gulp.task('scripts', function() {
+  return gulp
+    // sources of js files, any .js but not min.js files
+    .src(['js/*.js', '!js/*.min.js'])
+    // start stream
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(concat('scripts.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('js'))
+    .pipe(notify({ message: 'Scripts task complete, BrowserSync reloaded' }));
+});
+
+// Styles
+// initial task of generating one styles file with all sass imports
+gulp.task('glob:sass', function() {
+  return gulp
+    // path where files should be globbed from
+    .src('../sass/**/_*.scss', {cwd: 'sass/'})
+    .pipe(globbing({
+      // writes new file to the Changed Work Directory declared above
+      path: 'styles.scss'
+    },
+    {
+      useSingleQuotes: true
+    }
+  ))
+    .pipe(gulp.dest('sass/'));
+});
+
+// converts sass to css with gulp-sass, with dependency of glob:sass task
+gulp.task('sass', ['glob:sass'], function() {
+  return gulp
+    .src('sass/styles.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('css'))
+    .pipe(browserSync.reload({ stream: true })) // this is where the magic happens
+    .pipe(notify({ message: 'Styles task complete, BrowserSync reloaded' }));
+});
+
+
+// Watch
+gulp.task('watch', function() {
+  gulp.watch('sass/*/*.scss', ['sass']);
+  gulp.watch('js/*.js', ['scripts']);
+});
+
+// Serve
+gulp.task('serve', function() {
+  browserSync.init({
+    ui: false,
+    proxy: 'drupalville8.dev',
+    host: 'drupalville8.dev'
+  });
+  gulp.start('watch').on('change', browserSync.reload);
+});
+
+// Default task: command of 'gulp'
+gulp.task('default', ['serve'], function() {
+  gulp.start(['sass'], ['scripts']);
+});
