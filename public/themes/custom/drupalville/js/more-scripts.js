606d450b2e8cb6b8679f30f643c73dbfe3df4c29
Jin Trang <hu.trang@gmail.com>
2016-06-29 14:51:12 -0400

    added demo scripts to test for js minification

---
+++ public/themes/custom/drupalville/js/more-scripts.js
@@ -0,0 +1,7 @@
+function secondFunction() {
+  return "returning second function's value";
+}
+
+function thirdFunction() {
+  return "returning third function's value";
+}
---
+++ public/themes/custom/drupalville/js/scripts.js
@@ -0,0 +1,15 @@
+function myXFunction(){
+  var x = 20;
+  return x;
+}
+
+function myYFunction(){
+  var y = 25;
+  return y;
+}
+
+function myZFunction(){
+  var z = 250;
+  return z;
+}
+
